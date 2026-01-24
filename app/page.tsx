2652ac6fb9281a10f07ceb7a7b8dee32914c94d5
PawDevUK <pfsiwek@gmail.com>
2026-01-24 22:07:15 +0000

    fix: update welcome message and description in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
 	return (
 		<div className='min-h-screen flex flex-col mainPading text-center space-y-4 '>
-			<span className='text-amazon text-4xl font-bold mt-10'>Welcome to invoice fetcher</span>
-			<span className='text-amazon text-lg font-semibold'>Fetch invoices hassle free from multiple platforms</span>
+			<span className='text-amazon text-4xl font-bold mt-10'>Welcome to Profit Radar</span>
+			<span className='text-amazon text-lg font-semibold'>Track salvage car profits across platforms</span>
 		</div>
 	);
 }
