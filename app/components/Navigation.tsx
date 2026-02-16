0b7ead4fea43134ea0df98117dc4d77d1a5b0774
PawDevUK <p.f.siwek@gmail.com>
2026-02-16 16:49:33 +0000

    fix: remove platform data fetching from Navigation component

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,7 +1,5 @@
 import NavigationClient from './NavigationClient';
-import { getAllPlatforms } from '../../lib/platforms';
 
 export default async function Navigation() {
-	const platforms = await getAllPlatforms();
-	return <NavigationClient platforms={platforms} />;
+	return <NavigationClient />;
 }
