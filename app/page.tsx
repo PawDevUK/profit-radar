84a62b2671e87da72949bbbdc9766d5de8e9266b
PawDevUK <p.f.siwek@gmail.com>
2026-02-18 13:09:12 +0000

    feat: redirect Home component to HowItWorks page and remove welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-	return (
-		<div className='min-h-screen flex flex-col mainPading text-center space-y-4 '>
-			<span className='text-amazon text-4xl font-bold mt-10'>Welcome to Profit Radar</span>
-			<span className='text-amazon text-lg font-semibold'>Track salvage car profits across platforms</span>
-		</div>
-	);
+	redirect('/howItWorks');
+	return null;
 }
