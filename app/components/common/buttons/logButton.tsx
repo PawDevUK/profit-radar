a4d6452a36a1d98fe0cfb4ab67c8909cb6a25d99
PawDevUK <p.f.siwek@gmail.com>
2026-02-18 18:55:05 +0000

    feat: add onclick handler to LogButton for enhanced interactivity

--- app/components/common/buttons/logButton.tsx
+++ app/components/common/buttons/logButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 import './buttons.css';
-export default function LogButton({ item }: { item: { href: string; label: string } }) {
+export default function LogButton({ item, onclick }: { item: { href: string; label: string }; onclick?: () => void }) {
 	return (
 		<div className=''>
 			<Link key={item.href} href={item.href} className='log-button'>
-				{item.label}
+				<button onClick={onclick}>{item.label}</button>
 			</Link>
 		</div>
 	);
