706c73ef03221a7fa42a1347147ea72ab992a211
PawDevUK <p.f.siwek@gmail.com>
2026-02-21 13:36:39 +0000

    feat: add Model component with CollapseCard for filter display

---
+++ app/inventory/search/model.tsx
@@ -0,0 +1,10 @@
+'use client';
+import CollapseCard from '@/app/components/common/collapseCard/collapseCard';
+
+export default function Model() {
+	return (
+		<div>
+			<CollapseCard title='Model'>{'Model filter content goes here'}</CollapseCard>
+		</div>
+	);
+}
