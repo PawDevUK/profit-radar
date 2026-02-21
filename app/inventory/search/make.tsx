1fdd6ea9b4962f3f2662eb9190943a838372bdeb
PawDevUK <p.f.siwek@gmail.com>
2026-02-21 13:36:34 +0000

    feat: add Make component with CollapseCard for filter display

---
+++ app/inventory/search/make.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CollapseCard from '@/app/components/common/collapseCard/collapseCard';
+
+export default function Make() {
+	return (
+		<div>
+			<CollapseCard title='Make'>{'Make filter content goes here'}</CollapseCard>
+		</div>
+	);
+}
