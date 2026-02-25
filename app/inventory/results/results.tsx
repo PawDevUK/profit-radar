66286c88fdcc79c73625a98d10b564dc960da4c6
pawel <pfsiwek@gmail.com>
2026-02-25 20:16:55 +0000

    feat: enhance Results component to accept children prop and display results count

--- app/inventory/results/results.tsx
+++ app/inventory/results/results.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default function Results() {
+interface ResultsProps {
+	children: string[];
+}
+
+export default function Results({ children }: ResultsProps) {
 	return (
 		<div className='mx-1'>
 			<h1>Results</h1>
+			<h3>{children.length} results found</h3>
+			{children.map((car: string, i: number) => {
+				return <div key={i}>{car}</div>;
+			})}
 		</div>
 	);
 }
