8df11cc762b6980a7ceb8dd78d0fff8a4ebe4b86
PawDevUK <p.f.siwek@gmail.com>
2026-02-28 13:09:56 +0000

    fix: add urbanCamo image and export it in index.ts

--- img/index.ts
+++ img/index.ts
@@ -1,3 +1,4 @@
 export { default as bmw535 } from './bmw535.jpg';
 export { default as tiguan } from './tiguan.jpg';
 export { default as chargergt } from './chargergt.jpg';
+export { default as urbanCamo } from './urabnCamo.jpg';
