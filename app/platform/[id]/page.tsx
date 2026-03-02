1568297aaa6830e51da17c226c129af99ffa5547
PawDevUK <p.f.siwek@gmail.com>
2026-03-02 16:03:47 +0000

    fix: comment out Calendar component in PlatformPage

--- app/platform/[id]/page.tsx
+++ app/platform/[id]/page.tsx
@@ -38,7 +38,7 @@ export default async function PlatformPage({ params }: PageProps) {
 				</p>
 			</div>
 
-			<Calendar></Calendar>
+			{/* <Calendar></Calendar> */}
 
 			{/* Status & Login Section */}
 			<div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-6'>
