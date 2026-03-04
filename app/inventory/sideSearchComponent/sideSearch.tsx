1dc8b2b048f1b508ed7e36b4f9e52c5e8feaac42
PawDevUK <p.f.siwek@gmail.com>
2026-03-04 22:48:45 +0000

    feat: add icons to CheckBoxList components in SideSearch

--- app/inventory/sideSearchComponent/sideSearch.tsx
+++ app/inventory/sideSearchComponent/sideSearch.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import CheckBoxList from './search/checkBoxList';
+import { MapPin, Car, Gavel, Fuel, KeySquare, CarFront, List } from 'lucide-react';
+import './style.css';
 import {
 	makes,
 	titleType,
@@ -19,7 +21,57 @@ interface SideSearchProps {
 	filteredSaleResults: (cars: string[]) => void;
 	resetAll: boolean;
 }
+const size = 22;
+const color = '#4b5563';
+const stroke = 2;
 
+const BodyTypeIcon = () => {
+	return (
+		<svg
+			data-gui='atds-icon-body-type-filter'
+			xmlns='http://www.w3.org/2000/svg'
+			height={`${size}`}
+			width={`${size}`}
+			viewBox='0 0 24 24'
+			fill={`${color}`}
+			className='atds-icon-svg'>
+			<path d='M16 13H8V15H16V13Z'></path>
+			<path d='M21.79 10.1L20.94 8.41L19.98 4.68C19.82 3.71 18.99 3 18 3H5.98C5 3 4.17 3.7 4.03 4.59L3.06 8.42L2.21 10.11C2.07 10.39 2 10.69 2 11V19C2 20.1 2.9 21 4 21H6.01C7.11 21 8.01 20.1 8.01 19H16.01C16.01 20.1 16.91 21 18.01 21H20.01C21.11 21 22.01 20.1 22.01 19V11C22.01 10.69 21.94 10.38 21.8 10.1H21.79ZM19 13H20V19H18V17H6V19H4V13H5C6.1 13 7 12.1 7 11H4L4.89 9.21L5.98 5L18.03 5.09L19.03 9.01L20 11H17C17 12.1 17.9 13 19 13Z'></path>
+		</svg>
+	);
+};
+
+const Engine = () => {
+	return (
+		<svg
+			data-gui='atds-icon-engine-size-filter'
+			xmlns='http://www.w3.org/2000/svg'
+			height={`${size}`}
+			width={`${size}`}
+			viewBox='0 0 24 24'
+			fill={`${color}`}
+			className='atds-icon-svg'>
+			<title>Engine size filter</title>
+			<path d='M20 6H18V5C18 3.9 17.1 3 16 3H10C8.9 3 8 3.9 8 5V6H7C5.89 6 5 6.9 5 8V9H4V7H2V13H4V11H5V12C5 13.1 5.9 14 7 14H7.47L8.88 16.11C9.25 16.67 9.88 17 10.55 17H16.01C17.11 17 18.01 16.1 18.01 15V14H22.01V6H20.01H20ZM20 12H16V15H10.53L8.53 12H6.99V8H9.99V5H15.99V8H19.99V12H20Z'></path>
+			<path d='M21 16L19 18H20V19H4V18H5L3 16L1 18H2V19V20V21H22V20V19V18H23L21 16Z'></path>
+		</svg>
+	);
+};
+const Gears = () => {
+	return (
+		<svg
+			data-gui='atds-icon-transmission-filter'
+			xmlns='http://www.w3.org/2000/svg'
+			height={`${size}`}
+			width={`${size}`}
+			viewBox='0 0 24 24'
+			fill={`${color}`}
+			className='atds-icon-svg'>
+			<title>Transmission filter</title>
+			<path d='M21 5C21 3.9 20.1 3 19 3C17.9 3 17 3.9 17 5C17 5.74 17.4 6.38 18 6.72V11H13V6.72C13.6 6.37 14 5.74 14 5C14 3.9 13.1 3 12 3C10.9 3 10 3.9 10 5C10 5.74 10.4 6.38 11 6.72V11H6V6.72C6.6 6.37 7 5.74 7 5C7 3.9 6.1 3 5 3C3.9 3 3 3.9 3 5C3 5.74 3.4 6.38 4 6.72V17.27C3.4 17.62 3 18.25 3 18.99C3 20.09 3.9 20.99 5 20.99C6.1 20.99 7 20.09 7 18.99C7 18.25 6.6 17.61 6 17.27V12.99H11V17.27C10.4 17.62 10 18.25 10 18.99C10 20.09 10.9 20.99 12 20.99C13.1 20.99 14 20.09 14 18.99C14 18.25 13.6 17.61 13 17.27V12.99H18C19.1 12.99 20 12.09 20 10.99V6.72C20.6 6.37 21 5.74 21 5Z'></path>
+		</svg>
+	);
+};
 export default function SideSearch({ filteredSaleResults, resetAll }: SideSearchProps) {
 	const [selectedMakes, setSelectedMakes] = useState<string[]>([]);
 	const [selectedModels, setSelectedModels] = useState<string[]>([]);
@@ -64,19 +116,58 @@ export default function SideSearch({ filteredSaleResults, resetAll }: SideSearch
 
 	return (
 		<div className='flex flex-col '>
-			<CheckBoxList title='Make' options={makes} selected={selectedMakes} onChange={setSelectedMakes} scrollable />
-			<CheckBoxList title='Model' options={makes} selected={selectedModels} onChange={setSelectedModels} scrollable />
+			<CheckBoxList
+				title='Make'
+				options={makes}
+				selected={selectedMakes}
+				onChange={setSelectedMakes}
+				scrollable
+				icon={<KeySquare strokeWidth={stroke} className='checkboxIcon' />}
+			/>
+			<CheckBoxList
+				title='Model'
+				options={makes}
+				selected={selectedModels}
+				onChange={setSelectedModels}
+				scrollable
+				icon={<CarFront strokeWidth={stroke} className='checkboxIcon' />}
+			/>
 			<CheckBoxList title='Vehicle title type' options={titleType} selected={selectedTitleType} onChange={setSelectedTitleType} />
 			<CheckBoxList title='Vehicle condition type' options={conditionType} selected={selectedConditionType} onChange={setSelectedConditionType} />
 			<CheckBoxList title='Vehicle type' options={vehicleType} selected={selectedVehicleType} onChange={setSelectedVehicleType} scrollable />
-			<CheckBoxList title='Engine type' options={engineType} selected={selectedEngineType} onChange={setSelectedEngineType} />
-			<CheckBoxList title='Transmission' options={transmissionType} selected={selectedTransmissionType} onChange={setSelectedTransmissionType} />
-			<CheckBoxList title='Fuel type' options={fuelType} selected={selectedFuelType} onChange={setSelectedFuelType} />
+			<CheckBoxList title='Engine type' options={engineType} selected={selectedEngineType} onChange={setSelectedEngineType} icon={<Engine />} />
+			<CheckBoxList title='Transmission' options={transmissionType} selected={selectedTransmissionType} onChange={setSelectedTransmissionType} icon={<Gears />} />
+			<CheckBoxList
+				title='Fuel type'
+				options={fuelType}
+				selected={selectedFuelType}
+				onChange={setSelectedFuelType}
+				icon={<Fuel strokeWidth={stroke} className='checkboxIcon' />}
+			/>
 			<CheckBoxList title='Drive train' options={driveTrain} selected={selectedDriveTrain} onChange={setSelectedDriveTrain} />
 			<CheckBoxList title='Cylinder' options={cylinderType} selected={selectedCylinderType} onChange={setSelectedCylinderType} />
-			<CheckBoxList title='Auction name' options={auctionName} selected={selectedAuctionName} onChange={setSelectedAuctionName} />
-			<CheckBoxList title='Location' options={location} selected={selectedLocation} onChange={setSelectedLocation} scrollable />
-			<CheckBoxList title='Body style' options={bodyType} selected={selectedBodyType} onChange={setSelectedBodyType} />
+			<CheckBoxList
+				title='Auction name'
+				options={auctionName}
+				selected={selectedAuctionName}
+				onChange={setSelectedAuctionName}
+				icon={<Gavel strokeWidth={stroke} className='checkboxIcon' />}
+			/>
+			<CheckBoxList
+				title='Location'
+				options={location}
+				selected={selectedLocation}
+				onChange={setSelectedLocation}
+				scrollable
+				icon={<MapPin strokeWidth={stroke} className='checkboxIcon' />}
+			/>
+			<CheckBoxList
+				title='Body style'
+				options={bodyType}
+				selected={selectedBodyType}
+				onChange={setSelectedBodyType}
+				icon={<Car strokeWidth={stroke} className='checkboxIcon' />}
+			/>
 		</div>
 	);
 }
