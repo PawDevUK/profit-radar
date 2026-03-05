68bfbd62d4916214a281bc1f68bddb68c9306def
PawDevUK <p.f.siwek@gmail.com>
2026-03-05 11:43:26 +0000

    fix: update checkbox size and adjust icon positioning for improved UI

--- app/inventory/sideSearchComponent/search/checkBoxList.tsx
+++ app/inventory/sideSearchComponent/search/checkBoxList.tsx
@@ -48,11 +48,11 @@ export default function CheckBoxList({ options, selected, onChange, title, scrol
 								<input
 									type='checkbox'
 									checked={selected.includes(option)}
-									className='peer h-4 w-4 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 '
+									className='peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 '
 									onChange={() => handleChange(option)}
 								/>
-								<span className='absolute text-[var(--mongo-green)] opacity-0 peer-checked:opacity-100 top-[11px] left-[8px] transform -translate-x-1/2 -translate-y-1/2 pointer-events-none'>
-									<svg xmlns='http://www.w3.org/2000/svg' className='h-3.5 w-3.5' viewBox='0 0 20 20' fill='currentColor' stroke='currentColor' strokeWidth='1'>
+								<span className='absolute text-[var(--mongo-green)] opacity-0 peer-checked:opacity-100 top-[12px] left-[10px] transform -translate-x-1/2 -translate-y-1/2 pointer-events-none'>
+									<svg xmlns='http://www.w3.org/2000/svg' className='h-4.5 w-4.5' viewBox='0 0 20 20' fill='currentColor' stroke='currentColor' strokeWidth='1'>
 										<path
 											fillRule='evenodd'
 											d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
@@ -60,7 +60,7 @@ export default function CheckBoxList({ options, selected, onChange, title, scrol
 										/>
 									</svg>
 								</span>
-								<div className='ml-2 mt-0.5 text-gray-500'>{option}</div>
+								<div className='ml-2 mt-0.5 text-gray-500 text-[16px]'>{option}</div>
 							</label>
 						</div>
 					</div>
