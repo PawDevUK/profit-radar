55b30aa531ad2d37b695bd94fedc2482626ff369
pawel <pfsiwek@gmail.com>
2026-03-09 10:15:38 +0000

    feat: enhance Card component with mobile image display and improved layout

--- app/inventory/results/card/card.tsx
+++ app/inventory/results/card/card.tsx
@@ -3,33 +3,62 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
+const MobileImages = ({ item }: { item: React.ComponentProps<typeof Card>['item'] }) => {
+	return (
+		<div className='flex flex-row  w-full'>
+			<div className='w-2/3'>
+				<Image className='rounded-t-lg w-full' src={item.images?.[0] || '/placeholder-image.png'} alt='lot image' width={300} height={300} />
+			</div>
+			<div className='w-1/3 flex'>
+				<div className='w-1/2'>
+					<Image className='rounded-t-lg w-full' src={item.images?.[0] || '/placeholder-image.png'} alt='lot image' width={300} height={300} />
+				</div>
+				<div className='w-1/2'>
+					<Image className='rounded-t-lg w-full' src={item.images?.[0] || '/placeholder-image.png'} alt='lot image' width={300} height={300} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
 function Card({ ...props }) {
 	const { item } = props;
 	return (
-		<div className='w-full max-w-sm md:w-[30%] lg:w-[20%] xl:w-[18%] m-2.5 bg-neutral-primary-soft border border-gray-300 rounded-base shadow-md rounded-lg'>
-			<a href='#'>
+		<div className='md:max-h-112.5 w-full max-w-sm md:w-[30%] lg:w-[22%] xl:w-[18%] m-2.5 bg-neutral-primary-soft border border-gray-300 rounded-base shadow-md rounded-lg flex flex-col'>
+			{/* Show MobileImages only on mobile */}
+			<div className='md:hidden'>
+				<MobileImages item={item} />
+			</div>
+			{/* Show main image only on md and up */}
+			<a href='#' className='hidden md:block'>
 				<Image className='rounded-t-lg w-full' src={item.images?.[0] || '/placeholder-image.png'} alt='lot image' width={300} height={300} />
 			</a>
-			<div className='p-2'>
-				<div className='flex items-center space-x-3 mb-1'>
-					<span>Lot number:</span>
-					<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 rounded-sm'>{item.lotNumber}</span>
-				</div>
-				<a href='#'>
-					<h5 className='text-lg text-heading font-semibold tracking-tight'>{item.title}</h5>
-				</a>
-				<div className='flex flex-col items-center justify-between mt-6'>
-					<div className='flex flex-row items-center space-x-1 mb-1'>
-						<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 rounded-sm'>{item.odometer} miles</span>
-						{item.hasKey ? (
-							<span className='border border-brand-subtle text-green-600  text-xs font-medium px-1.5 py-0.5 rounded-sm'>keys</span>
-						) : (
-							<span className='border border-brand-subtle text-red-600  text-xs font-medium px-1.5 py-0.5 rounded-sm'>no keys</span>
-						)}
-						<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 rounded-sm'>{item.year}</span>
+
+			<div className='p-2 flex flex-col justify-between flex-1'>
+				<div className='flex flex-col'>
+					<div className=' flex items-center space-x-3 mb-1'>
+						<span>Lot number:</span>
+						<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 rounded-sm'>{item.lotNumber}</span>
 					</div>
-					<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 rounded-sm'>{item.primaryDamage}</span>
-					<div className='flex flex-row space-x-4'>
+					<a className='' href='#'>
+						<h5 className='text-lg text-heading font-semibold tracking-tight'>{item.title}</h5>
+					</a>
+				</div>
+				<div className='flex flex-col items-center mt-1 '>
+					<div className='flex flex-col'>
+						<div className=' flex flex-row items-center space-x-1 mb-1'>
+							<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 rounded-sm'>{item.odometer} miles</span>
+							{item.hasKey ? (
+								<span className='border border-brand-subtle text-green-600  text-xs font-medium px-1.5 py-0.5 rounded-sm'>keys</span>
+							) : (
+								<span className='border border-brand-subtle text-red-600  text-xs font-medium px-1.5 py-0.5 rounded-sm'>no keys</span>
+							)}
+							<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 rounded-sm'>{item.year}</span>
+						</div>
+						<span className='border border-brand-subtle text-(--main-blue)  text-xs font-medium px-1.5 py-0.5 m-auto rounded-sm'>{item.primaryDamage}</span>
+					</div>
+
+					<div className='flex flex-row space-x-4 mt-1'>
 						<div className='flex flex-col'>
 							<span className='text-[12px]'>Current Bid</span>
 							<span className='text-xl font-extrabold text-heading'>${item.currentBid}</span>
@@ -39,20 +68,6 @@ function Card({ ...props }) {
 							<span className='text-xl font-extrabold text-heading'>${item.buyItNow}</span>
 						</div>
 					</div>
-					{/* <button
-						type='button'
-						className='inline-flex items-center  text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none'>
-						<svg className='w-4 h-4 me-1.5' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'>
-							<path
-								stroke='currentColor'
-								stroke-linecap='round'
-								stroke-linejoin='round'
-								stroke-width='2'
-								d='M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312'
-							/>
-						</svg>
-						Add to cart
-					</button> */}
 				</div>
 			</div>
 		</div>
