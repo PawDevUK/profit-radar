20e2b8b6675291672eb992140bc373921dd27ad4
pawel <pfsiwek@gmail.com>
2026-03-12 12:35:41 +0000

    fix: integrate selectOne_State and filter results based on selected filter

--- app/saleListResults/[id]/page.tsx
+++ app/saleListResults/[id]/page.tsx
@@ -5,7 +5,10 @@ import { useState, useEffect, useRef } from 'react';
 import saleList from '@/app/results/saleList.json';
 import { SaleList } from '@/lib/types/saleList';
 
+import { selectOne_State } from '@/lib/state/searchFilters';
+
 export default function SaleListResultsPage() {
+	const { selectedOneFilter } = selectOne_State();
 	const params = useParams();
 	const router = useRouter();
 	const saleId = params.id as string;
@@ -17,6 +20,14 @@ export default function SaleListResultsPage() {
 	const [saleMeta, setSaleMeta] = useState<{ location?: string; saleDate?: string; saleTime?: string } | null>(null);
 	const fetchingRef = useRef(false); // Prevent duplicate fetching
 
+	const filterResults = (selected: string, cars: SaleList[]) => {
+		return cars.filter((car) => car.make === selected);
+	};
+
+	useEffect(() => {
+		console.log(selectedOneFilter);
+	}, [selectedOneFilter]);
+
 	return (
 		<div className='w-full min-h-screen bg-gray-50 py-3 px-4 sm:px-6 lg:px-8'>
 			<div className='max-w-(--max-app-width) mx-auto'>
@@ -26,7 +37,7 @@ export default function SaleListResultsPage() {
 					))}
 				</div>
 
-				{cars.length === 0 && (
+				{filterResults(selectedOneFilter.make, cars).length === 0 && (
 					<div className='text-center py-12'>
 						<p className='text-gray-500 text-lg'>No cars found for this sale</p>
 					</div>
