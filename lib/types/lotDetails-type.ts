b34a38610357cdd791d11ec3d4e55ddeab290fe6
pawel <pfsiwek@gmail.com>
2026-03-17 09:17:06 +0000

    fix: update attribute names for drivetrain and fuel in lot details for consistency

--- lib/types/lotDetails-type.ts
+++ lib/types/lotDetails-type.ts
@@ -6,6 +6,7 @@ export interface LotDetails {
 	make: string;
 	model: string;
 	trim: string;
+	bodyStyle: string;
 	runAndDrive: boolean;
 	vin: string;
 	lotNumber: number;
@@ -18,25 +19,26 @@ export interface LotDetails {
 	transmissionEngages: boolean;
 	transmissionNote: string; // e.g. "Copart verified that the transmission engages."
 	titleCode: string; // e.g. "PA - Cert Of Title"
-	titleStatus: string; // e.g. "Title Absent"
+	vehicleTitleType: string; // e.g. "Title Absent"
 	odometer: number;
 	odometerUnit: string;
 	odometerStatus: string; // e.g. "Not Actual"
 	primaryDamage: string; // e.g. "Normal Wear"
-	cylinders: number;
+	cylinders: string;
 	color: string;
 	hasKey: boolean;
 	engineType: string; // e.g. "2.0L 4"
 	transmission: string; // e.g. "Automatic"
 	vehicleType: string; // e.g. "Medium Duty/box Trucks"
-	drivetrain: string; // e.g. "Rear-wheel drive"
-	fuel: string; // e.g. "Diesel"
+	driveTrain: string; // e.g. "Rear-wheel drive"
+	fuelType: string; // e.g. "Diesel"
 	saleDate: string; // e.g. "Thu. Feb 05, 2026 02:00 AM GMT"
 	highlights: string[];
 	notes: string;
 	lastUpdated: string; // e.g. "02/03/2026 12:27 am"
 	currentBid: number;
 	buyItNow: number | null;
+	auctionName: string;
 	auctionCountdown: string; // e.g. "0D 16H 10min"
 	images: string[]; // image URLs
 }
