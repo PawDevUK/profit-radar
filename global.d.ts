a4d1b207db7f5f46552f43dc8d17bdeee264bebb
pawel <pfsiwek@gmail.com>
2026-03-22 20:02:08 +0000

    feat: add Google Maps type definitions and update global.d.ts for Google Maps integration

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,10 @@
+/// <reference types="google.maps" />
+
 declare module '*.css' {
 	const content: { [className: string]: string };
 	export default content;
+}
+
+interface Window {
+	google: typeof google;
 }
