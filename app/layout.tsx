df09d4461e36f5107a6033fe9b07a8afca39ffe3
PawDevUK <p.f.siwek@gmail.com>
2026-03-23 13:08:57 +0000

    fix: add suppressHydrationWarning to body for improved rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 			<head>
 				<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' />
 			</head>
-			<body>
+			<body suppressHydrationWarning>
 				<Navigation />
 
 				{/* Main Content */}
