48fcc99230aa67b45c45229989e4836b717e1232
pawel <pfsiwek@gmail.com>
2026-03-23 15:09:09 +0000

    feat: add SearchBar component to Locations page for improved location search functionality

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -2,12 +2,14 @@
 import Map from '@/app/locations/map';
 import LocationsList from '@/app/locations/locationsList';
 import { useState } from 'react';
+import SearchBar from '@/app/components/search/search';
 
 export default function Locations() {
 	const [selectedLocation, setSelectedLocation] = useState<string | undefined>(undefined);
 
 	return (
 		<div className='flex flex-row h-full'>
+			<SearchBar></SearchBar>
 			<Map selectedLocation={selectedLocation}></Map>
 			<LocationsList
 				selectLocation={(location: string) => {
