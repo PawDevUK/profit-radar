907d537e09d890b6e49eef6b3e1a3dbe4141b30f
pawel <pfsiwek@gmail.com>
2026-03-24 10:15:14 +0000

    fix: update Locations component to improve layout and integrate SearchBar with location options

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,20 +1,22 @@
 'use client';
 import Map from '@/app/locations/map';
-import LocationsList from '@/app/locations/locationsList';
+// import LoctionsList from '@/app/locations/locationsList';
+import { fixedLocationCoordinates } from '@/app/locations/locations';
 import { useState } from 'react';
 import SearchBar from '@/app/components/search/search';
 
 export default function Locations() {
+	const returnlocationArray = (locations: typeof FixedLocationCoordinates) => {
+		return Object.keys(locations);
+	};
 	const [selectedLocation, setSelectedLocation] = useState<string | undefined>(undefined);
 
 	return (
-		<div className='flex flex-row h-full'>
-			<SearchBar></SearchBar>
+		<div className='flex flex-col h-full w-full relative'>
+			<div className='z-10 flex flex-row absolute top-15 left-2 w-[90%]'>
+				<SearchBar placeholderText='Search location' options={returnlocationArray(fixedLocationCoordinates)} handleOnChange={(query) => {}}></SearchBar>
+			</div>
 			<Map selectedLocation={selectedLocation}></Map>
-			<LocationsList
-				selectLocation={(location: string) => {
-					setSelectedLocation(location);
-				}}></LocationsList>
 		</div>
 	);
 }
