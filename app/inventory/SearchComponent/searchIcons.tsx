eef6e26d8a20e7b8285877ab9d78a91c6c852a36
pawel <pfsiwek@gmail.com>
2026-03-27 13:13:34 +0000

    fix: adjust SVG group transformation and path for VehicleType icon

--- app/inventory/SearchComponent/searchIcons.tsx
+++ app/inventory/SearchComponent/searchIcons.tsx
@@ -65,9 +65,9 @@ const VehicleType = () => {
 			<circle cx='7' cy='17' r='2' />
 			<path d='M9 17h6' />
 			<circle cx='17' cy='17' r='2' />
-			<g transform='translate(-2,-7)'>
+			<g transform='translate(-2,-3)'>
 				<path d='M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3' />
-				<path d='M12 17h.01' />
+				<path d='M12 16h.01' />
 			</g>
 		</svg>
 	);
