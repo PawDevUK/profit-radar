990549761a10ac94b0aab3cf414ed5709986a145
pawel <pfsiwek@gmail.com>
2026-03-27 13:08:33 +0000

    fix: remove reset button and adjust button styles for improved layout

--- app/inventory/SearchComponent/collapseCard/collapseCard.tsx
+++ app/inventory/SearchComponent/collapseCard/collapseCard.tsx
@@ -2,15 +2,12 @@
 import React, { useState } from 'react';
 import './style.css';
 
-export default function CollapseCard({ children, title, resetOptions, icon }: { children: React.ReactNode; title: string; resetOptions?: () => void; icon?: React.ReactNode }) {
+export default function CollapseCard({ children, title, icon }: { children: React.ReactNode; title: string; icon?: React.ReactNode }) {
 	const [open, setOpen] = useState(false);
 
 	return (
 		<div className='z-20 bg-white borderBottom'>
-			<button className='absolute right-15 top-2 resetButton' onClick={resetOptions}>
-				Reset
-			</button>
-			<button className='w-full flex items-center justify-between px-5 py-2 font-semibold text-black focus:outline-none' onClick={() => setOpen((prev) => !prev)}>
+			<button className='w-full flex items-center justify-between py-5 px-10 font-semibold text-black focus:outline-none' onClick={() => setOpen((prev) => !prev)}>
 				<div className='flex flex-row'>
 					{icon && <span className='icon'>{icon}</span>}
 					<span className='header ml-2'>{title}</span>
@@ -18,8 +15,8 @@ export default function CollapseCard({ children, title, resetOptions, icon }: { 
 				<svg
 					id='chevron'
 					className={`transition-transform duration-200 ${open ? 'rotate-180' : ''}`}
-					width='23'
-					height='23'
+					width='30'
+					height='30'
 					fill='none'
 					stroke='currentColor'
 					strokeWidth='2'
