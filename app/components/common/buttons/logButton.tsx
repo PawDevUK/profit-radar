4c22a22e03718c0a5362c5e6afe1fcdc6d9edad9
pawel <pfsiwek@gmail.com>
2026-04-09 11:52:45 +0100

    fix: add class prop to LogButton for improved styling flexibility

--- app/components/common/buttons/logButton.tsx
+++ app/components/common/buttons/logButton.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import Link from 'next/link';
 import './buttons.css';
-export default function LogButton({ item, onclick }: { item: { href: string; label: string; fontSize?: number }; onclick?: () => void }) {
+
+export default function LogButton({ item, onclick }: { item: { href: string; label: string; fontSize?: number; class?: string }; onclick?: () => void }) {
 	return (
-		<div className=''>
-			<Link key={item.href} href={item.href} className='log-button' style={item.fontSize ? { fontSize: `${item.fontSize}px` } : undefined}>
-				<button onClick={onclick}>{item.label}</button>
+		<div className='' onClick={onclick}>
+			<Link key={item.href} href={item.href} className={`${item.class ? item.class : ``} log-button`} style={item.fontSize ? { fontSize: `${item.fontSize}px` } : undefined}>
+				<button>{item.label}</button>
 			</Link>
 		</div>
 	);
