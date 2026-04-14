ae2c8c9a145acf47c7b498ff7f5331d1ca424302
PawDevUK <p.f.siwek@gmail.com>
2026-04-14 11:44:56 +0100

    feat: refactor scrapeLot function to handle multiple URLs and improve data extraction logic

--- lib/scrapers/copart/lot/lotScraper.ts
+++ lib/scrapers/copart/lot/lotScraper.ts
@@ -1,58 +1,92 @@
 import puppeteer, { type GoToOptions } from 'puppeteer';
 import convertLotImgURL from './parseImgUrls.js';
 import scraperHTMLtags from './AI_HTML_extract/AIresponse.json' with { type: 'json' };
-const pageUrl = 'https://www.copart.com/lot/99763515/salvage-2017-subaru-wrx-dc-washington-dc';
 
 const pageOptions: GoToOptions = {
 	waitUntil: 'networkidle0',
 	timeout: 0,
 };
 
-export default async function scrapeLot(pageUrl: string) {
+export default async function scrapeLot(pageUrls: string[] | string) {
+	async function getData(pageUrls: string) {
+		await page.goto(pageUrls, pageOptions);
+		console.log('Launching page:', pageUrls);
+		await page.content();
+		let lotObj = {};
+		let Success = 0;
+		let failedScraped = 0;
+		const failedSelectors = [];
+
+		for (const field of Object.values(scraperHTMLtags.fields)) {
+			if (field.selector) {
+				try {
+					Success += 1;
+					await page.waitForSelector(field.selector, { timeout: 1000 });
+					const value = await page.$eval(field.selector, (el) => el.textContent?.trim() ?? '');
+					const label = field.label || '';
+					lotObj = { ...lotObj, [label]: value };
+					if (field.label !== 'Images') {
+						lotObj = { ...lotObj, [label]: value };
+					}
+				} catch {
+					// selector not present within 1 second, skip to next field
+					failedScraped += 1;
+					failedSelectors.push(field.selector);
+					continue;
+				}
+			} else if (!field.selector) {
+				failedScraped += 1;
+			}
+		}
+		console.log('Scrapped', Success, 'elements.');
+		console.log('Failed', failedScraped, 'elements.');
+		console.log('Finished scrapping info!!');
+		console.log('Starting scrapping images.');
+
+		// extract image URLs
+		const imageUrls = await page.$$eval('.img-responsive.p-galleria-img-thumbnail', (images) => images.map((img) => img.getAttribute('src') || '').filter(Boolean));
+		if (imageUrls.length > 0) {
+			console.log('Image URLs extracted successfully');
+		}
+		const convertedImage = convertLotImgURL(imageUrls);
+
+		return {
+			scrapedLotObj: {
+				...lotObj,
+				Images: convertedImage,
+			},
+			scrapingInfo: {
+				Success,
+				Failed: failedScraped,
+				failedSelectors,
+				Url: pageUrls.length > 1 ? pageUrls[0] : pageUrls,
+			},
+		};
+	}
 	const options = {
 		headless: false,
 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
 	};
+
 	const browser = await puppeteer.launch(options);
 	const page = await browser.newPage();
-	await page.goto(pageUrl, pageOptions);
-	console.log('Launching page:', pageUrl);
-	await page.content();
-
-	let lotObj = {};
-	let count = 0;
-	let failedScraped = 0;
-	for (const field of Object.values(scraperHTMLtags.fields)) {
-		if (field.selector) {
-			count += 1;
-			await page.waitForSelector(field.selector);
-			const value = await page.$eval(field.selector, (el) => el.textContent?.trim() ?? '');
-			const label = field.label || '';
-			lotObj = { ...lotObj, [label]: value };
-			if (field.label !== 'Images') {
-				lotObj = { ...lotObj, [label]: value };
-			}
-		} else if (!field.selector) {
-			failedScraped += 1;
+	let scrapedData = {};
+	if (pageUrls && typeof pageUrls === 'string') {
+		const pageUrl = pageUrls;
+		scrapedData = await getData(pageUrl);
+	} else if (Array.isArray(pageUrls)) {
+		console.log('Number of Urls to scrape -- >', pageUrls.length);
+		for (const url of pageUrls) {
+			const Data = await getData(url);
+			scrapedData = {
+				...scrapedData,
+				Data,
+			};
 		}
 	}
-	console.log('Scrapped', count, 'elements.');
-	console.log('Failed', failedScraped, 'elements.');
-	console.log('Finished scrapping info!!');
-	console.log('Starting scrapping images.');
 
-	// extract image URLs
-	const imageUrls = await page.$$eval('.img-responsive.p-galleria-img-thumbnail', (images) => images.map((img) => img.getAttribute('src') || '').filter(Boolean));
-	if (imageUrls.length > 0) {
-		console.log('Image URLs extracted successfully');
-	}
-	const convertedImage = convertLotImgURL(imageUrls);
-	const scrapedLotObj = {
-		...lotObj,
-		Images: convertedImage,
-	};
 	console.log('Finalising and closing');
 	await browser.close();
 	console.log('----Scrapper closed!----');
-	return scrapedLotObj;
+	return scrapedData;
 }
