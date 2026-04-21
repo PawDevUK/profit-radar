b1fe23fd00c8ccb0f649fd76e8eb80fe6afe3dc7
PawDevUK <p.f.siwek@gmail.com>
2026-04-21 10:05:10 +0100

    Updated types.

--- lib/types/lotDetails-type.ts
+++ lib/types/lotDetails-type.ts
@@ -7,16 +7,16 @@ export interface LotDetails {
 	model: string | null;
 	trim: string | null;
 	bodyStyle: string | null;
-	runAndDrive: string | null;
+	runAndDrive: boolean | null;
 	vin: string | null;
 	lotNumber: string | null;
 	laneItem: string | null; // e.g. "-/-"
 	saleName: string | null;
 	location: string | null;
-	engineVerified: string | null;
+	engineVerified: boolean | null;
 	engineVerifiedNote: string | null; // e.g. "Copart verified that the engine starts."
 	engineStatus: string | null; // e.g. "Engine Starts"
-	transmissionEngages: string | null;
+	transmissionEngages: boolean | null;
 	transmissionNote: string | null; // e.g. "Copart verified that the transmission engages."
 	titleCode: string | null; // e.g. "PA - Cert Of Title"
 	vehicleTitleType: string | null; // e.g. "Title Absent"
@@ -26,14 +26,14 @@ export interface LotDetails {
 	primaryDamage: string | null; // e.g. "Normal Wear"
 	cylinders: string | null;
 	color: string | null;
-	hasKey: string | null;
+	hasKey: boolean | null;
 	engineType: string | null; // e.g. "2.0L 4"
 	transmission: string | null; // e.g. "Automatic"
 	vehicleType: string | null; // e.g. "Medium Duty/box Trucks"
 	driveTrain: string | null; // e.g. "Rear-wheel drive"
 	fuelType: string | null; // e.g. "Diesel"
 	saleDate: string | null; // e.g. "Thu. Feb 05, 2026 02:00 AM GMT"
-	highlights: string | null;
+	highlights: string[] | null;
 	notes: string | null;
 	lastUpdated: string | null; // e.g. "02/03/2026 12:27 am"
 	currentBid: string | null;
