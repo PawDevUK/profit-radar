3599f7f1f1d7796337b5d636775dc96926086f58
PawDevUK <p.f.siwek@gmail.com>
2026-04-22 14:26:00 +0100

    Added check if the model is existing and  cashed. If not it is created.

--- lib/db/models.ts
+++ lib/db/models.ts
@@ -1,6 +1,6 @@
-import { model, Schema } from 'mongoose';
+import { model, models, Schema } from 'mongoose';
 import { MonthSaleSchema } from '@/lib/db/schema';
 
-const MonthSaleModel = model('MonthSale', MonthSaleSchema as Schema);
+const MonthSaleModel = models.MonthSale || model('MonthSale', MonthSaleSchema as Schema);
 
 export default MonthSaleModel;
