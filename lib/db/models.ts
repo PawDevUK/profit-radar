bdc13ec494388af61ecf01569abc68eb53e353f2
PawDevUK <p.f.siwek@gmail.com>
2026-04-22 14:53:50 +0100

    fix: add optional chaining to models reference in MonthSaleModel

--- lib/db/models.ts
+++ lib/db/models.ts
@@ -1,6 +1,6 @@
 import { model, models, Schema } from 'mongoose';
 import { MonthSaleSchema } from '@/lib/db/schema';
 
-const MonthSaleModel = models.MonthSale || model('MonthSale', MonthSaleSchema as Schema);
+const MonthSaleModel = models?.MonthSale || model('MonthSale', MonthSaleSchema as Schema);
 
 export default MonthSaleModel;
