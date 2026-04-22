2b2886960c18dbfa54384645eca2b3d167c06cdb
pawel <pfsiwek@gmail.com>
2026-04-22 20:07:28 +0100

    fix: update console log message and correct prop name for Calendar component

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -22,7 +22,7 @@ export default function CalendarPage() {
 					.then((data) => {
 						if (data) {
 							setSales(data);
-							console.log('Calendar data fetched', data);
+							console.log('Calendar data fetched!!');
 						} else {
 							setError('No Calendar fetched from the Database');
 						}
@@ -39,7 +39,7 @@ export default function CalendarPage() {
 
 	return (
 		<div className='min-h-screen bg-gray-50 py-6 px-4 sm:px-6 lg:px-8'>
-			<Calendar sales={sales} />
+			<Calendar allAuctions={sales} />
 		</div>
 	);
 }
