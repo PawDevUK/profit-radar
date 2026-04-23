adda6f8b5570cf1c276a2bdc84851d5643c82594
PawDevUK <p.f.siwek@gmail.com>
2026-04-23 20:51:58 +0100

    feat: refactor saleListScraper to accept dynamic URL and list size parameters

--- lib/scrapers/copart/saleList/saleListScraper.ts
+++ lib/scrapers/copart/saleList/saleListScraper.ts
@@ -4,21 +4,18 @@ import puppeteer, { Browser, Page, type GoToOptions } from 'puppeteer';
 import { proxyConfig, ProxyConfig } from '@/lib/scrapers/proxy/proxy-config';
 import scrapeLot from '../lot/lotScraper';
 
-const salesListUrl =
-	'https://www.copart.com/saleListResult/10/2026-04-16?location=CA%20-%20Los%20Angeles&saleDate=1776366000000&liveAuction=false&from=%2FsalesListResult&yardNum=10&qId=1a813ad6-7f82-46c2-bf32-efdc71e1a1f1-1776157018819';
-const scrapedListSizeNum = 30;
 const pageOptions: GoToOptions = {
 	waitUntil: 'networkidle0',
 	timeout: 0,
 };
-export default async function saleListScraper() {
+export default async function saleListScraper(saleUrl: string, scrapedListSizeNum: number) {
 	const options = {
 		headless: false,
 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
 	};
 	const browser = await puppeteer.launch(options);
 	const salesPage = await browser.newPage();
-	await salesPage.goto(salesListUrl, pageOptions);
+	await salesPage.goto(saleUrl, pageOptions);
 	await salesPage.waitForSelector('a[aria-label="Lot Details"]', { timeout: 30000 });
 
 	const lotUrls = await salesPage.$$eval('a[aria-label="Lot Details"]', (anchors) => anchors.map((a) => (a as HTMLAnchorElement).href).filter(Boolean));
@@ -26,14 +23,8 @@ export default async function saleListScraper() {
 	console.log('Found lot URLs:', reducedNumOfUrls);
 
 	const scrapedSaleList = await scrapeLot(reducedNumOfUrls);
-	fs.writeFile('./scrapedSaleList.json', JSON.stringify(scrapedSaleList), 'utf-8', (err) => {
-		if (err) console.error('Error writing file:', err);
-	});
-	await browser.close();
-}
 
-saleListScraper();
-// I need to set puppeteer to get nested url form nested element
-// p-d-flex p-flex-column ng-star-inserted
-// ng-star-inserted
-// <a> aria-label="Lot Details"
+	await browser.close();
+
+	return scrapedSaleList;
+}
