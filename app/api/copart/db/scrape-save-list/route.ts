615ffebc601372c03b7b89722364ff235ede31a0
PawDevUK <p.f.siwek@gmail.com>
2026-04-24 14:24:45 +0100

    feat: implement PUT endpoint for scraping and saving sales list data

---
+++ app/api/copart/db/scrape-save-list/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from 'next/server';
+import saleListScraper from '@/lib/scrapers/copart/saleList/saleListScraper';
+import { getAllSalesLists } from '@/lib/db/db';
+import { format, isPast } from 'date-fns';
+import { saveSalesList } from '@/lib/db/db';
+import { SaleListType } from '@/lib/types/calendar-type';
+import { scrapedLotDataType } from '@/lib/types/lotDetails-type';
+
+export async function PUT(NextRequest) {
+	const allDBdata = await getAllSalesLists();
+	const auctions = allDBdata[0].auctions;
+	const currentSales: SaleListType[] = [];
+	const scraping = false;
+
+	auctions.map((auction: SaleListType) => {
+		if (auction.currentSale ? !isPast(auction.currentSale) : null) {
+			currentSales.push(auction);
+		}
+	});
+
+	async function scrapeAndSave(sale: SaleListType, _id: string) {
+		let arrayOfScrapedLots;
+		let scrapedData;
+		console.log('Starting scraping auctions.');
+		if (sale.currentSaleUrl && _id) {
+			scrapedData = await saleListScraper(sale.currentSaleUrl, null);
+			console.log('Finished scrapping action.');
+			if (Array.isArray(scrapedData)) {
+				arrayOfScrapedLots = scrapedData.map((lot: scrapedLotDataType) => lot.scrapedLotObj);
+				await saveSalesList(_id, arrayOfScrapedLots);
+			}
+		}
+		if (scrapedData) {
+			return NextResponse.json({
+				message: 'Scraped and saved sales list success!!',
+				data: arrayOfScrapedLots,
+			});
+		}
+	}
+	const sale = currentSales[1];
+	const saleId = sale._id;
+	const saleUrl = sale.currentSaleUrl;
+	console.log(sale);
+
+	// currentSales.map((sale) => {
+	// 	const saleUrl = sale.currentSaleUrl;
+	try {
+		if (saleUrl) {
+			scrapeAndSave(sale, saleId);
+		} else {
+			console.log('Not scraped any data');
+		}
+	} catch (e) {
+		console.log(e);
+	}
+	// });
+}
