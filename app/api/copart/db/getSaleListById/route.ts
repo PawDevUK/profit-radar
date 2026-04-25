eee25d24930b849080eb00241fc7b7da227c7e5a
PawDevUK <p.f.siwek@gmail.com>
2026-04-25 12:59:19 +0100

    fix: remove console log from GET function in route.ts

--- app/api/copart/db/getSaleListById/route.ts
+++ app/api/copart/db/getSaleListById/route.ts
@@ -1,10 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
-
 import { getOneSalesList } from '@/lib/db/db';
 
 export async function GET(request: NextRequest) {
 	const id = request.nextUrl.searchParams.get('id');
-	console.log(id);
 
 	const fetchedSaleList = await getOneSalesList(id ? id : '');
 	if (fetchedSaleList) {
