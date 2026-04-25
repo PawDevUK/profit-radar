f6f6ff1c2b395b31aaad04e8d1b4df959b3d3227
pawel <pfsiwek@gmail.com>
2026-04-25 13:40:56 +0100

    fix: wrap saleListScraper logic in try-catch for better error handling

--- lib/scrapers/copart/saleList/saleListScraper.ts
+++ lib/scrapers/copart/saleList/saleListScraper.ts
@@ -11,15 +11,18 @@ export default async function saleListScraper(saleUrl: string, scrapedListSizeNu
 		headless: false,
 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
 	};
-	if (saleUrl) {
-		const browser = await puppeteer.launch(options);
-		const salesPage = await browser.newPage();
-		await salesPage.goto(saleUrl, pageOptions);
-		await salesPage.waitForSelector('a[aria-label="Lot Details"]', { timeout: 30000 });
-		const lotUrls = await salesPage.$$eval('a[aria-label="Lot Details"]', (anchors) => anchors.map((a) => (a as HTMLAnchorElement).href).filter(Boolean));
-		const scrapedSaleList = await scrapeLot(scrapedListSizeNum ? lotUrls.slice(0, scrapedListSizeNum) : lotUrls);
-		await browser.close();
-		return scrapedSaleList;
+	try {
+		if (saleUrl) {
+			const browser = await puppeteer.launch(options);
+			const salesPage = await browser.newPage();
+			await salesPage.goto(saleUrl, pageOptions);
+			await salesPage.waitForSelector('a[aria-label="Lot Details"]', { timeout: 30000 });
+			const lotUrls = await salesPage.$$eval('a[aria-label="Lot Details"]', (anchors) => anchors.map((a) => (a as HTMLAnchorElement).href).filter(Boolean));
+			const scrapedSaleList = await scrapeLot(scrapedListSizeNum ? lotUrls.slice(0, scrapedListSizeNum) : lotUrls);
+			await browser.close();
+			return scrapedSaleList;
+		}
+	} catch (e) {
+		console.log(e);
 	}
-	return 'No sales Urls or list to scrape!!';
 }
