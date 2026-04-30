9fc202929488cc7773fe3bfcc33b451efa0de0a6
pawel <pfsiwek@gmail.com>
2026-04-30 22:26:32 +0100

    fix: update tempUrl for Copart login route to reflect new sale details

--- app/api/copart/logIn/route.ts
+++ app/api/copart/logIn/route.ts
@@ -2,9 +2,9 @@ import { logInToCopart } from '@/lib/scrapers/copart/logIn/logIn';
 
 import { NextResponse } from 'next/server';
 const tempUrl =
-	'https://www.copart.com/saleListResult/127/2026-04-30?location=PA%20-%20York%20Haven&saleDate=1777557600000&liveAuction=false&from=%2FsalesListResult&yardNum=127&qId=1a813ad6-7f82-46c2-bf32-efdc71e1a1f1-1777495209260';
-const landingUrl = 'https://www.copart.com';
+	'https://www.copart.com/saleListResult/376/2026-05-01?location=OH%20-%20Akron&saleDate=1777644000000&liveAuction=false&from=%2FsalesListResult&yardNum=376&qId=e2abaa7e-31cb-4be0-bb78-93292d1d8101-1777578609594';
 
+const landingUrl = 'https://www.copart.com';
 export async function GET() {
 	await logInToCopart(landingUrl, tempUrl);
 	return NextResponse.json({ data: 'Success!' });
