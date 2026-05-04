0ea702ee653cd182d309d0918f9eb4d271a5883a
PawDevUK <p.f.siwek@gmail.com>
2026-05-04 23:06:45 +0100

    refactor: enhance GET function to handle CSV conversion and save lots

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,14 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { convertJsonSalesList } from '@/lib/csvToLotDetails/jsonToLotDetails';
+import { convertCSVtoJSON } from '@/lib/csvToLotDetails/csvToJson';
 import { v4 as uuidv4 } from 'uuid';
+import output from '@/lib/csvToLotDetails/output.json';
+import { saveLots } from '@/lib/db/db';
 
 export async function GET(req: NextRequest) {
 	const { searchParams } = new URL(req.url);
+
 	const id = searchParams.get('id'); // e.g. /api?id=123
-	const uuid = uuidv4();
-	const converted = await convertJsonSalesList(uuid);
+	const csvSalePath = searchParams.get('csvSalePath');
+	let converted;
+	let dataSavedStatus;
+
+	if (csvSalePath && id) {
+		const output = await convertCSVtoJSON(csvSalePath);
+		converted = await convertJsonSalesList(id, output);
+	} else {
+		console.log('There is some issue with conversion!');
+		console.log('id          ----', id);
+		console.log('csvSalePath ----', csvSalePath);
+		return NextResponse.json({
+			message: 'There is no path to the file or sale id!!',
+		});
+	}
+	if (converted.length > 0) {
+		dataSavedStatus = await saveLots(converted);
+	}
+	console.log(converted.length);
 	return NextResponse.json({
 		data: converted,
 		message: '',
+		dataSavedStatus,
 	});
 }
