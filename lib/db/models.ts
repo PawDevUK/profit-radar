8d5cdfb9412ba85da015044256b78a6b0f085f36
PawDevUK <p.f.siwek@gmail.com>
2026-05-04 23:09:11 +0100

    refactor: rename LotDetails to LotDetailsModel for consistency in model naming

--- lib/db/models.ts
+++ lib/db/models.ts
@@ -2,4 +2,4 @@ import { model, models, Schema } from 'mongoose';
 import { CalendarSaleSchema, LotDetailsSchema } from '@/lib/db/schema';
 
 export const CalendarSaleModel = models?.CalendarSale || model('CalendarSale', CalendarSaleSchema as Schema);
-export const LotDetails = models?.LotDetails || model('LotDetails', LotDetailsSchema as Schema);
+export const LotDetailsModel = models?.LotDetails || model('LotDetails', LotDetailsSchema as Schema);
