631d22dce49688533dbcaf25d0cc531bc96eb366
PawDevUK <p.f.siwek@gmail.com>
2026-05-05 10:30:59 +0100

    feat: implement GET endpoint to fetch all lots from the database

---
+++ app/api/copart/db/getAllLots/route.ts
@@ -0,0 +1,20 @@
+import { getAllLots } from '@/lib/db/db';
+import { NextResponse } from 'next/server';
+
+export const runtime = 'nodejs';
+
+export async function GET() {
+	try {
+		const data = await getAllLots();
+		return NextResponse.json(
+			{
+				message: data.length ? 'OK' : 'No Calendar fetched from the Database',
+				data,
+			},
+			{ status: 200 },
+		);
+	} catch (error) {
+		console.error('getAllSaleLists GET failed:', error);
+		return NextResponse.json({ error: 'Failed to fetch sale lists' }, { status: 500 });
+	}
+}
