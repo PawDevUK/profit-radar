9817c7be7684b1ea9df04e1ece41fb70b29c36c1
PawDevUK <p.f.siwek@gmail.com>
2026-05-14 15:13:25 +0100

    refactor: remove unused state and components from Page, streamline filter toggle logic

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { lazy, memo, Suspense, useState, useTransition } from 'react';
-import { X, SlidersHorizontal } from 'lucide-react';
+import { memo, Suspense } from 'react';
+import { SlidersHorizontal } from 'lucide-react';
 import SaleListResultsPage from './saleListResults/[id]/page';
 import SearchChipsFilters from './searchChipsFilters/searchChipsFilters';
-const SideSearch = lazy(() => import('./SearchComponent/Search'));
 const MemoizedSaleListResultsPage = memo(SaleListResultsPage);
 
+import { useSetSearchOpen } from '@/lib/state/searchFilters.state';
 const ToggleButton = ({ toggleFilters }: { toggleFilters: () => void }) => {
 	const IconSize = 22;
 	return (
@@ -16,24 +16,11 @@ const ToggleButton = ({ toggleFilters }: { toggleFilters: () => void }) => {
 	);
 };
 
-const CloseButton = ({ toggleFilters }: { toggleFilters: () => void }) => {
-	return (
-		<button className='w-8 h-8' onClick={toggleFilters}>
-			{<X id='closeButton' className='ml-2 checkboxIcon' />}
-		</button>
-	);
-};
-
 export default function Page() {
-	const [open, setOpen] = useState(false);
-	const [, startTransition] = useTransition();
-
+	const toggleOpenSearch = useSetSearchOpen();
 	const toggleFilters = () => {
-		startTransition(() => {
-			setOpen((prev) => !prev);
-		});
+		toggleOpenSearch();
 	};
-	const resetAllFilters = function () {};
 
 	return (
 		<div className='relative min-h-screen flex flex-col'>
@@ -46,26 +33,6 @@ export default function Page() {
 						<ToggleButton toggleFilters={toggleFilters} />
 					</div>
 				</div>
-				{open ? (
-					<aside className='flex flex-col relative z-20 left-0 -top-50 w-[calc(100vw-20px)] lg:w-150 mx-auto h-[calc(100vh-30px)] bg-white rounded-lg shadow-lg md:shadow-none overflow-y-auto'>
-						<div className='px-10 py-6 flex items-center justify-between'>
-							<h2 className='text-[22px] font-bold text-(--header-text)'>Filter and sort</h2>
-							<div className='flex items-center gap-2'>
-								<button onClick={resetAllFilters} className='resetButton'>
-									Reset All
-								</button>
-								<CloseButton toggleFilters={toggleFilters} />
-							</div>
-						</div>
-						<Suspense fallback={<div className='p-4 text-gray-500'>Loading filters...</div>}>
-							<SideSearch />
-						</Suspense>
-					</aside>
-				) : (
-					<div></div>
-				)}
-
-				{open && <div className='fixed  inset-0 z-10 bg-black/50' onClick={() => startTransition(() => setOpen(false))} />}
 			</div>
 			<div className='w-full  md:ml-0 '>
 				<Suspense fallback={<div className='p-4 text-gray-500'>Loading inventory...</div>}>
