865d74997d9ee64ddffa48938801f05426ace3b0
PawDevUK <p.f.siwek@gmail.com>
2026-05-19 10:54:56 +0100

    feat: add initial GET function for updating calendar from CSV

---
+++ app/api/copart/db/updateDatabaseFromCsv/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET() {
+	// Update calendar
+	// Get calendar and loop over it to get urls
+	// Send request with url and id to get csv
+	// return NextResponse.json({});
+}
