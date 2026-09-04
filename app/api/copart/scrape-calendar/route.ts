c9a4f7485d0252f223295e9b2170144141223000
PawDevUK <p.f.siwek@gmail.com>
2026-09-04 14:42:56 +0100

    fix: handle request signal in calendar scraping function

--- app/api/copart/scrape-calendar/route.ts
+++ app/api/copart/scrape-calendar/route.ts
@@ -4,13 +4,16 @@ import { updateCalendar } from '@/lib/db/db';
 
 export async function GET(request: Request) {
 	try {
-		const scrapedCalendar = await scrapeCopartCalendar();
-		const saveResponse = await updateCalendar(scrapedCalendar);
-		return NextResponse.json({
-			status: 200,
-			headers: { 'content-type': 'application/json' },
-			...saveResponse,
-		});
+		const signal = request.signal;
+		const scrapedCalendar = await scrapeCopartCalendar(signal);
+		if (!signal.aborted) {
+			const saveResponse = await updateCalendar(scrapedCalendar);
+			return NextResponse.json({
+				status: 200,
+				headers: { 'content-type': 'application/json' },
+				...saveResponse,
+			});
+		}
 	} catch (err: unknown) {
 		const errorMessage = err instanceof Error ? err.message : String(err);
 		return new Response(JSON.stringify({ error: errorMessage }), {
